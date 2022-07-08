18121c8f5da736582a41165a0dd8547737366ad2
KORsinsa <worldwidemind9@gmail.com>
2022-07-08 14:18:29 +0900

    hard to do!

---
+++ hello.py
@@ -0,0 +1,20 @@
+print('hello')
+for i in range(10):
+    if i % 3 == 0:
+        print('year')
+    else: print(i)
+
+for i in range(20):
+    if i % 3 == 0:
+        print('year')
+    elif i % 5 ==0:
+        print('dream')
+    elif i % 15==0:
+        print('yeardream')
+    else:
+        print(i)
+
+
+
+
+
